84e22c6ed243476d35c56a5bfa954b89cbfd4b15
darren.a.jackson <darren.a.jackson@accenture.com>
2016-03-24 18:18:34 +0000

    updates to docker_image_inspector script

--- imageTest/image_inspector/docker_image_inspector.py
+++ imageTest/image_inspector/docker_image_inspector.py
@@ -9,7 +9,8 @@ found_list = []
 
 # print message
 def print_this(mystring, level):
-    info_level = 2 # 0 is off, 1 is info, 2+ is debug
+
+    info_level = 1 # 0 is off, 1 is info, 2+ is debug
     if info_level >= 2 and level == 2:
         print "DEBUG: " + str(mystring)
     elif info_level >= 1 and level == 1:
@@ -17,6 +18,7 @@ def print_this(mystring, level):
 
 # read in parameters
 def main(argv):
+
     image_name = ''
     inspect_file = ''
     usage = 'docker_inspect.py -i <image_name> -f <inspect_file>'
@@ -46,6 +48,7 @@ def main(argv):
 
 # process the inspection
 def docker_inspect(myimage, myfile):
+
     # connect to the docker daemon
     cli = docker.Client(base_url='unix://var/run/docker.sock', tls=True)
     # collect inspect data for image
@@ -69,26 +72,57 @@ def read_file(thisfile):
 # inspect dictionary
 def inspect_dict(e, d, p):
 
-    print_this("Starting dictionary inspection...", 2)
     global expected_list
     global found_list
+    print_this("Starting dictionary inspection...", 2)
+#    print_this("Exp: dictionary size is: " + str(len(e)), 2)
+#    print_this("Fnd: dictionary size is: " + str(len(d)), 2)
+#    if len(e) > len(d):
+#        for k, v in e.iteritems():
+#            if not k in d:
+#                #print "# stuff missing from the new images= " + str(p) + "-" + str(k) + "-" + str(v)
+#                print_this("WARNING - missing item in new docker image: " + str(p) + "-" + str(k) + "-" + str(v), 1)
+#    elif len(e) < len(d):
+#        for k, v in d.iteritems():
+#            if not k in e:
+#                #print "# additional stuff found in new image (kinda expected)= " + str(p) + "-" + str(k) + "-" + str(v)
+#                print_this("INFO - additional item found in new docker image: " + str(p) + "-" + str(k) + "-" + str(v), 2)
 
     for k, v in e.iteritems():
-        if isinstance(v, dict) and v:
-            p2 = p + "-" + k
-            d2 = d[k]
-            inspect_dict(v, d2, p2)
-        elif isinstance(v, list):
-            p2 = p + "-" + k
-            d2 = d[k]
-            inspect_array(v, d2, p2)
+
+        if k in d:
+            if isinstance(v, dict) and v:
+                p2 = p + "-" + k
+                d2 = d[k]
+                big_dictionary_inspection(e[k], d[k], p2)
+                inspect_dict(v, d2, p2)
+            elif isinstance(v, list):
+                p2 = p + "-" + k
+                d2 = d[k]
+                inspect_array(v, d2, p2)
+            else:
+                print_this("Exp: " + str(p) + "-{0}={1}".format(k, v), 2)
+                print_this("Fnd: " + str(p) + "-" + str(k) + "=" + str(d[k]), 2)
+                expected_string = str(p) + "-{0}={1}".format(k, v)
+                found_string = str(p) + "-" + str(k) + "=" + str(d[k])
+                expected_list.append(expected_string)
+                found_list.append(found_string)
         else:
-            print_this("Exp: " + str(p) + "-{0}={1}".format(k, v), 2)
-            print_this("Fnd: " + str(p) + "-" + str(k) + "=" + str(d[k]), 2)
-            expected_string = str(p) + "-{0}={1}".format(k, v)
-            found_string = str(p) + "-" + str(k) + "=" + str(d[k])
-            expected_list.append(expected_string)
-            found_list.append(found_string)
+            print_this("Skipping item: " + k + " this should have been reported as missing from the new docker images already!", 1)
+
+# compare two dictionaries
+def big_dictionary_inspection(de, dd, dp):
+    print_this("Exp: dictionary size is: " + str(len(de)), 2)
+    print_this("Fnd: dictionary size is: " + str(len(dd)), 2)
+    if len(de) > len(dd):
+        for k, v in de.iteritems():
+            if not k in dd:
+                print_this("WARNING - missing item in new docker image: " + str(dp) + "-" + str(k) + "-" + str(v), 1)
+    elif len(de) < len(dd):
+        for k, v in dd.iteritems():
+            if not k in de:
+                print_this("INFO - additional item found in new docker image: " + str(dp) + "-" + str(k) + "-" + str(v), 1)
+
 
 # inspect array
 def inspect_array(ae, ad, ap):
@@ -109,6 +143,7 @@ def inspect_array(ae, ad, ap):
 
 # compare two lists
 def compare_lists(el, dl):
+
     print_this("Starting list comparison...", 2)
     for i in range(len(el)):
         if not el[i] == dl[i]:
@@ -118,6 +153,7 @@ def compare_lists(el, dl):
 
 # inspect array bae for additional elements
 def big_array_inspection(a1, stringI, a2, stringP):
+
     global expected_list
     global found_list
     a1_dict = {}
